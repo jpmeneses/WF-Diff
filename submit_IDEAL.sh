f4473569aaf430959f852c360001627b9eccd66e
jpmeneses <jpmeneses@uc.cl>
2023-08-02 10:43:03 +1000

    Submit file: Initial submission of DDPM-DL model, 200 timesteps (default), train for 30 epochs

---
+++ submit_IDEAL.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=v000-DDPM
+#SBATCH --output=out_DDPM_000.txt
+#SBATCH --partition=gpus
+#SBATCH --gres=gpu:quadro_rtx_8000:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jpmeneses@uc.cl	
+
+python train-ddpm.py --experiment_dir 'DDPM-000' --epochs 30 --epoch_ckpt 5
