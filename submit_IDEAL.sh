4c56336e6c587c2e4383190b7f6c39aef97d17d9
jpmeneses <jpmeneses@uc.cl>
2023-08-02 15:03:58 +1000

    Re-submit DDPM(v001): Train until achieve 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-DDPM
-#SBATCH --output=out_DDPM_002.txt
+#SBATCH --job-name=v001-DDPM
+#SBATCH --output=out_DDPM_001.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ddpm.py --experiment_dir 'DDPM-002' --batch_size 4 --epochs 30 --epoch_ckpt 5 --data_augmentation False
+python train-ddpm.py --experiment_dir 'DDPM-001' --batch_size 2 --epochs 100 --epoch_ckpt 5
