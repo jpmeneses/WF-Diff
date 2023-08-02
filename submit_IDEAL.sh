3975acf56421f3dacdff63e45dbdfc48fe2501d9
jpmeneses <jpmeneses@uc.cl>
2023-08-02 23:18:32 +1000

    Submit DDPM(v003): Train for 100 epochs with batch size 8 and without data augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-DDPM
-#SBATCH --output=out_DDPM_002.txt
+#SBATCH --job-name=v003-DDPM
+#SBATCH --output=out_DDPM_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ddpm.py --experiment_dir 'DDPM-002' --batch_size 4 --epochs 100 --epoch_ckpt 5
+python train-ddpm.py --experiment_dir 'DDPM-003' --batch_size 8 --epochs 100 --epoch_ckpt 10
