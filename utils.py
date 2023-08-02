6a2ae6b91c30dda74100e7abfc8e0fb1d75da744
jpmeneses <jpmeneses@uc.cl>
2023-08-02 10:35:23 +1000

    Accessory functions

---
+++ utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+
+def make_space_above(axes, topmargin=1):
+    """ increase figure size to make topmargin (in inches) space for 
+        titles, without changing the axes sizes"""
+    fig = axes.flatten()[0].figure
+    s = fig.subplotpars
+    w, h = fig.get_size_inches()
+
+    figh = h - (1-s.top)*h  + topmargin
+    fig.subplots_adjust(bottom=s.bottom*h/figh, top=1-topmargin/figh)
+    fig.set_figheight(figh)
