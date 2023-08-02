59a476cd8212a7340fee37918b2c62699f17d0fd
jpmeneses <jpmeneses@uc.cl>
2023-08-02 11:20:24 +1000

    Remove GPU memory growth

--- tf2lib/__init__.py
+++ tf2lib/__init__.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 from tf2lib.utils import *
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-for d in physical_devices:
-    tf.config.experimental.set_memory_growth(d, True)
+# for d in physical_devices:
+#     tf.config.experimental.set_memory_growth(d, True)
